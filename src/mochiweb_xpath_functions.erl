f9ebff025a9a0c6dc9e00570b508ff12b903aa35
daniel <daniel.parming@matmax.es>
2016-10-26 12:53:51 +0200

    Fix substring function once and for all

--- src/mochiweb_xpath_functions.erl
+++ src/mochiweb_xpath_functions.erl
@@ -1,5 +1,5 @@
 %% xpath_functions.erl
-%% @author Pablo Polvorin 
+%% @author Pablo Polvorin
 %% @doc Some core xpath functions that can be used in xpath expressions
 %% created on 2008-05-07
 -module(mochiweb_xpath_functions).
@@ -16,9 +16,9 @@
 %% @type XPathArgSpec = XPathType | WildCardArgSpec
 %% @type WildCardArgSpec = {'*', XPathType}
 %% @type XPathType = node_set | string | number | boolean
-%% 
+%%
 %% The engine is responsable of calling the function with
-%% the correct arguments, given the function signature. 
+%% the correct arguments, given the function signature.
 -spec lookup_function(atom()) -> mochiweb_xpath:xpath_fun_spec() | false.
 lookup_function('last') ->
     {'last',fun last/2,[]};
@@ -49,18 +49,18 @@ lookup_function('string') ->
 lookup_function(_) ->
     false.
 
-%% @doc Function: int last() 
+%% @doc Function: int last()
 %%      The last function returns the context size of the current node
 last({ctx, _, _, _, _, Size} = _Ctx, []) ->
     Size.
 
-%% @doc Function: number position() 
+%% @doc Function: number position()
 %%      The position function returns the position of the current node
 position({ctx, _, _, _, Position, _} = _Ctx, []) ->
     Position.
 
-%% @doc Function: number count(node-set) 
-%%      The count function returns the number of nodes in the 
+%% @doc Function: number count(node-set)
+%%      The count function returns the number of nodes in the
 %%      argument node-set.
 count(_Ctx,[NodeList]) ->
     length(NodeList).
@@ -75,7 +75,7 @@ concat(_Ctx, BinariesList) ->
 'name'(_Ctx,[[{Tag,_,_,_}|_]]) ->
     Tag.
 
-%% @doc Function: boolean starts-with(string, string) 
+%% @doc Function: boolean starts-with(string, string)
 %%      The starts-with function returns true if the first argument string
 %%      starts with the second argument string, and otherwise returns false.
 'starts-with'(_Ctx,[Left,Right]) ->
@@ -85,7 +85,7 @@ concat(_Ctx, BinariesList) ->
         _ -> false
     end.
 
-%% @doc Function: boolean ends-with(string, string) 
+%% @doc Function: boolean ends-with(string, string)
 %%      The ends-with function returns true if the first argument string
 %%      ends with the second argument string, and otherwise returns false.
 'ends-with'(_Ctx,[Left,Right]) ->
@@ -105,32 +105,37 @@ contains(_Ctx,[Where, What]) ->
             true
     end.
 
-%% @doc Function: string substring(string, number, number?) 
-%%      The substring function returns the substring of the first argument 
+%% @doc Function: string substring(string, number, number?)
+%%      The substring function returns the substring of the first argument
 %%      starting at the position specified in the second argument with length
 %%      specified in the third argument
+substring(_Ctx,[<<>>,_Start,_Length]) -> <<>>;
 substring(_Ctx,[String,Start,Length]) when is_binary(String)->
-    Before = Start -1,
-    After = size(String) - Before - Length,
-    case (Start + Length) =< size(String) of
+    Before = Start - 1,
+    Size = size(String),
+    Max = Size - Before,
+    case (Start + Length) =< size(String) + 1 of
         true ->
+            After = Size - Before - Length,
             <<_:Before/binary,R:Length/binary,_:After/binary>> = String,
             R;
         false ->
-            <<>>
+            After = 0,
+            <<_:Before/binary,R:Max/binary,_:After/binary>> = String,
+            R
     end.
 
-%% @doc Function: number sum(node-set) 
-%%      The sum function returns the sum, for each node in the argument 
+%% @doc Function: number sum(node-set)
+%%      The sum function returns the sum, for each node in the argument
 %%      node-set, of the result of converting the string-values of the node
 %%      to a number.
 sum(_Ctx,[Values]) ->
     lists:sum([mochiweb_xpath_utils:number_value(V) || V <- Values]).
 
-%% @doc Function: number string-length(string?) 
-%%      The string-length returns the number of characters in the string 
+%% @doc Function: number string-length(string?)
+%%      The string-length returns the number of characters in the string
 %%      TODO: this isn't true: currently it returns the number of bytes
-%%            in the string, that isn't the same 
+%%            in the string, that isn't the same
 'string-length'(_Ctx,[String]) ->
     size(String).
 
