fc1647e942cbd23ba5d347b5fddc84cf82a2478c
daniel <daniel.parming@matmax.es>
2017-01-17 13:12:05 +0100

    Fix split function

--- src/mochiweb_xpath_functions.erl
+++ src/mochiweb_xpath_functions.erl
@@ -204,10 +204,9 @@ sum(_Ctx,[Values]) ->
 
 %% @doc Function: node-set split(string, string)
 %%      Split a string into nodes
-split(_Ctx,[<<>>,Separator]) -> [];
+split(_Ctx,[<<>>,_Separator]) -> [];
 split(_Ctx,[String,Separator]) ->
-    NodeList = string:tokens(binary_to_list(String),binary_to_list(Separator)),
-    lists:map(fun(Node) -> list_to_binary(Node) end, NodeList).
+    re:split(String, Separator, [{return, list}]).
 
 %% @doc Function: string join(node-set, string)
 %%      Split a string into nodes
